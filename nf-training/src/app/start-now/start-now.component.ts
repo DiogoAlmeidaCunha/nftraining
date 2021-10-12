7bc1dc17b9bc184048cf874d16193e2e329629b7
Diogo Almeida Cunha <diogocunha588@gmail.com>
2021-10-12 23:10:16 +0100

    fixed landing page

--- nf-training/src/app/start-now/start-now.component.ts
+++ nf-training/src/app/start-now/start-now.component.ts
@@ -12,4 +12,22 @@ export class StartNowComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showBox(n){
+    if(n == 1){
+      document.getElementById('bg1').style.backgroundImage = "url('../../assets/start22.jpg')";
+    }
+    if(n == 2){
+      document.getElementById('bg1').style.backgroundImage = "url('../../assets/start33.jpg')";
+    }
+  }
+
+  closeBox(v){
+    if(v == 1){
+      document.getElementById('bg1').style.backgroundImage = "url('../../assets/start9.jpg')";
+    }
+    if(v == 2){
+      document.getElementById('bg1').style.backgroundImage = "url('../../assets/start9.jpg')";
+    }
+  }
+
 }
